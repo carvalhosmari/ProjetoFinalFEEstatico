116fda320858ac92f68b43b9e8316958f41fe8df
carvalhosmari <mscarvall@gmail.com>
2022-11-01 16:53:02 -0300

    update do código js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 let form = document.querySelector("form");
-            form.addEventListener('submit', (e) => {
-                e.preventDefault();
-                let data = new FormData(form);
-                fetch('https://script.google.com/macros/s/AKfycbwi0qnvglMm49nMNrfXLGm67jDduhiFWCe2WC_FJqrPJ_rPXXuQ9kQCtDuBQQOhKNEZ/exec', {
-                    method: "POST",
-                    body: data
-                })
-                    .then(res => res.text())
-                    .then(data => { 
-                        document.querySelector("#msg").innerHTML = "Sucesso em seu envio"
-                        document.querySelector("#sub").value = "Enviado";
-                    });
-            });
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    document.querySelector("#sub").textContent = "Enviado";
+    let data = new FormData(form);
+    fetch('https://script.google.com/macros/s/AKfycbzsBw7cbM5VM4I8v00LBJYk2s6FoMaaRTQtbzmE3LDNXJXIcu0HHN8egbV1ZGrKR5yu/exec', {
+            method: "POST",
+            body: data
+        })
+        .then(res => res.text())
+        .then(data => {
+            // document.querySelector("#confirmacao").innerHTML = data;
+            document.querySelector("#sub").value = "Solicitado"
+        });
+})
